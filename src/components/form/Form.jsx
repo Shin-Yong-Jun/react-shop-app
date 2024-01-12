9f8c92d3df891fb9371ca7ea995aaf95c6b33f59
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2024-01-12 22:05:58 +0900

    파이어베이스 연결, 회원가입 로그인 UI 반영

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import styles from './Form.module.css'
 
-const Form = () => {
+const Form = ({title}) => {
   return (
-    <div>Form</div>
-  )
-}
+    <form className={styles.form}>
+      <div>
+        <input 
+          type="email"
+          placeholder="E-mail"
+        />
+        <div>
+          <span className={styles.form_error}></span>
+        </div>
+      </div>
+
+      <div>
+        <input 
+          type="password"
+          placeholder="Password"
+        />
+        <div>
+          <span className={styles.form_error}></span>
+        </div>
+      </div>
+      <button type="submit">{title}</button>
+      <span className={styles.form_error}></span>
+    </form>
+  );
+};
 
 export default Form
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrnkSQNHz2R8vjTGenXyEIs_CqTL7obtM",
+  authDomain: "react-shop-app-35561.firebaseapp.com",
+  projectId: "react-shop-app-35561",
+  storageBucket: "react-shop-app-35561.appspot.com",
+  messagingSenderId: "1000538844133",
+  appId: "1:1000538844133:web:1a2b7865450ba0ada75d0c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
