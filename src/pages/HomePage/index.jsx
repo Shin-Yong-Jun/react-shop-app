4d7622ee6968ff8c8ee307f29e024ff008a45044
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2024-01-15 10:38:46 +0900

    HomePage를 구성하기 위한 컴포넌트 폴더 파일 준비

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import CardList from '../../components/CardList'
+import CardSkeleton from '../../components/CardSkeleton'
+import CountProducts from '../../components/CountProducts'
+import FiltersCategory from '../../components/FiltersCategory'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="page">
+      <div className="container">
+        <h1>Products</h1>
+        <FiltersCategory />
+        <CountProducts />
+        <CardList />
+        <CardSkeleton />
+      </div>
+    </div>
   )
 }
 
