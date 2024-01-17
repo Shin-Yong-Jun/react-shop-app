2be584808c37b716a779772f967fc12a9a1f3118
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2024-01-17 13:58:43 +0900

    장바구니 기능 생성하기

--- src/pages/RegisterPage/sign-up/SignUp.jsx
+++ src/pages/RegisterPage/sign-up/SignUp.jsx
@@ -24,6 +24,7 @@ const SignUp = () => {
         token: userCredential.user.refreshToken,
         id: userCredential.user.uid
       }))
+      dispatch(setUserId(userCredential.user.uid));
       navigate('/');
     })
     .catch((error) => {
