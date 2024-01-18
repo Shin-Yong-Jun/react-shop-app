907d7b293b2102eb240332a610ffba66c45ac2f7
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2024-01-18 20:50:14 +0900

    Nav 컴포넌트 기능, 로그아웃과 카트 담을때 카트담은 아이템 숫자 표시 반영

--- src/components/header/nav/Nav.jsx
+++ src/components/header/nav/Nav.jsx
@@ -3,8 +3,35 @@ import { Link } from "react-router-dom";
 import { FiLogIn, FiShoppingCart, FiUser } from "react-icons/fi";
 import { GoSignOut } from "react-icons/go";
 import styles from "./Nav.module.scss";
+import { useAuth } from "../../../hooks/useAuth";
+import {signOut} from "firebase/auth";
+import app from "../../../firebase";
+import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
+import { removeUser } from "../../../store/user/user.slice";
+import { removeUserId } from "../../../store/cart/cart.slice";
+import { getAuth } from "firebase/auth";
 
 const Nav = () => {
+
+  const {isAuth} = useAuth();
+  const dispatch = useAppDispatch()
+  //firebase app을 가져와야 함
+  const auth = getAuth(app);
+  const {products} = useAppSelector((state) => state.cartSlice);
+
+
+  const handleSignOut = () => {
+    signOut(auth)
+    .then(() => {
+      dispatch(removeUser());
+      dispatch(removeUserId());
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+
   return (
     <nav className={styles.nav}>
       <ul>
@@ -14,6 +41,7 @@ const Nav = () => {
               {" "}
               <FiShoppingCart />
             </Link>
+            {products.length > 0 && <b>{products.length}</b>}
           </div>
         </li>
         <li>
@@ -25,13 +53,17 @@ const Nav = () => {
           </div>
         </li>
         <li>
+          {isAuth ? 
           <GoSignOut 
             className={styles.sign_out}
+            onClick={handleSignOut}
             title="로그아웃"
           />
+        :
           <Link to={"/login"}>
             <FiLogIn title="로그인" />
           </Link>
+        }
         </li>
       </ul>
     </nav>
