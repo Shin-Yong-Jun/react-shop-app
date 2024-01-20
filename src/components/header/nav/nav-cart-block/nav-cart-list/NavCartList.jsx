8d83202437d3133654c2fffc2bc5cc99957e3741
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2024-01-20 21:00:27 +0900

    navCartBlock 컴포넌트 생성완료, 3개가 이어짐

--- src/components/header/nav/nav-cart-block/nav-cart-list/NavCartList.jsx
+++ src/components/header/nav/nav-cart-block/nav-cart-list/NavCartList.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import { useAppSelector } from '../../../../../hooks/redux'
+import styles from './NavCartList.module.scss'
+import NavCartItem from './nav-cart-item/NavCartItem'
 
 
 const NavCartList = () => {
+  const {products} = useAppSelector((state) => state.cartSlice);
+
+
   return (
-    <div>NavCartList</div>
+    <div className={styles.nav_cart_list}>
+      {products.map((item) => (
+        <NavCartItem key={item.id} item={item}/>
+      ))}
+
+    </div>
   )
 }
 
