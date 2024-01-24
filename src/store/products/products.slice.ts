4d7c33eff5e44d1a310a5762402a32c005f415db
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2024-01-24 17:58:10 +0900

    리덕스 슬라이스 ts로 변환 중

--- src/store/products/products.slice.ts
+++ src/store/products/products.slice.ts
@@ -1,25 +1,33 @@
+import { IProduct } from './products.type';
 import { createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const fetchProducts = createAsyncThunk(
   "products/fetchProducts",
-  async (category, thunkAPI) => {
+  async (category: string, thunkAPI) => {
     try {
       let response;
       if(category) {
-        response = await axios.get(`https://fakestoreapi.com/products/category/${category}`)
+        response = await axios.get<IProduct[]>(`https://fakestoreapi.com/products/category/${category}`)
       } else {
-        response = await axios.get("https://fakestoreapi.com/products")
+        response = await axios.get<IProduct[]>("https://fakestoreapi.com/products")
       }
 
       return response.data; // payload
     } catch (e) {
-      return thunk.rejectWithValue("Error Loading products");
+      return thunkAPI.rejectWithValue("Error Loading products");
     }
   }
 )
 
-const initialState = {
+type ProductsType = {
+  products: IProduct[];
+  isLoading: boolean;
+  error: string;
+
+}
+
+const initialState: ProductsType = {
   products: [],
   isLoading: false,
   error: "",
@@ -41,9 +49,9 @@ export const productsSlice = createSlice({
         state.isLoading = false;
         state.products = action.payload;
       })
-      .addCase(fetchProducts.rejected, (state)=> {
+      .addCase(fetchProducts.rejected, (state,action)=> {
         state.isLoading = false;
-        state.error = action.payload;
+        state.error = action.payload as string;
       })
   }
 })
