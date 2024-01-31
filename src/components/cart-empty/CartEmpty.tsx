f9c731cb5aaa473416da4a5a6fdf8f9580b83aaa
Shin-Yong-Jun <wjstlsdyd123@gmail.com>
2024-01-31 15:57:34 +0900

    파이어베이스 전달테스트

--- src/components/cart-empty/CartEmpty.tsx
+++ src/components/cart-empty/CartEmpty.tsx
@@ -10,7 +10,7 @@ const CartEmpty:FC<CartEmptyProps> = ({title}) => {
   return (
     <div className={styles.cart_empty}>
       <img src="img/empty-cart.png" alt="cart empty" />
-      <h1>{title}가(이) 비어있습니다.</h1>
+      <h1>{title}가(이) 비어있다!.</h1>
       <p>{title}에 상품을 넣어주세요.</p>
       <Link to='/'>계속 쇼핑하기</Link>
     </div>
